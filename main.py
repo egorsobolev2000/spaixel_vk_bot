06ea4173c1dcdc7266de1f563858824930ec9592
egorsobolev2000 <web-disain@mail.ua>
2020-05-03 17:18:47 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import vk_api
 
 from config import TOKEN
 
-from vk_api.longpoll import VkLongPoll
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 from brain.main_handler import main_handler
 from debug.color import ColorsPrint
@@ -18,10 +18,12 @@ if __name__ == "__main__":
         os.system('clear')
         print(ColorsPrint('Запускаем бота...', 'att').do_colored())
         try:
-            main_handler(vk, longpoll)
+            print(f'\nСоединение с Дред — ', ColorsPrint('OK', 'suc').do_colored())
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    main_handler(vk, event)
         except Exception as e:
             print(ColorsPrint('Не удалось запустить бота', 'err').do_colored(), e)
-            raise e
         except KeyboardInterrupt:
             print(ColorsPrint('\n\nЗавершение соединения с ботом...', 'inf').do_colored())
             break
