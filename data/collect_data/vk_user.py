832bc461b301fa73f1aae77d80a62a99f3df3443
egorsobolev2000 <web-disain@mail.ua>
2020-05-03 17:16:12 +0300

    Класс для работы с объектом ПОЛЬЗОВАТЕЛЬ

---
+++ data/collect_data/vk_user.py
@@ -0,0 +1,22 @@
+class VkUser:
+    """ Класс для работы с объектом Пользователь"""
+
+    def __init__(self, vk):
+        self.vk = vk
+
+    def get_user_info(self, event, field=False, return_user=False):
+        """ Функция получает информацию о пользователе и возвращает
+            либо конкретное поле, либо весь словарь пользователя """
+
+        user = self.vk.users.get(user_ids=event.user_id,
+                                 fields='verified, sex, bdate, city, country,'
+                                        'domain, contacts, site,'
+                                        'followers_count, followers_count, counters,'
+                                        'relation, connections')[0]
+
+        if field:
+            need_field: object = user[field]
+            return need_field
+        if return_user:
+            return user
+
