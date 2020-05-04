50ee4812731d230d4f153b5a4f9183a7c18de9b1
egorsobolev2000 <web-disain@mail.ua>
2020-05-04 11:43:10 +0300

    Подключил обработку модуля FAQ

--- brain/user/main_user_handler.py
+++ brain/user/main_user_handler.py
@@ -1,3 +1,4 @@
+from body.user.modules.faq.user_m_f_keyboard import faq_keyboard
 from brain.base import random_id
 from debug.debug import log_error
 
@@ -5,14 +6,15 @@ from data.collect_data.vk_user import VkUser as User
 from data.collect_data.data_collect import data_collector
 
 from body.base_btn import KBB__BASE__MAIN_MENU
+from body.user.modules.faq.user_m_f_btns import *
 from body.user.modules.service.user_m_s_btns import *
 from body.user.modules.menu.s_keyboard.user_m_m_s_btns import *
 
 
 from data.modules_data.service.service_data import SERVICE_DATA
+from data.modules_data.faq.faq_data import FAQ_DATA
 
 from body.base import get_empty_keyboard
-from body.user.modules.faq.user_m_f_keyboard import faq_inline_keyboard
 from body.user.modules.service.user_m_s_keyboard import service_keyboard
 from body.user.modules.menu.s_keyboard.user_m_m_s_keyboard import standard_keyboard
 
@@ -62,14 +64,28 @@ def main_user_handler(vk, event):
             random_id=random_id()
         )
 
+    # Если человек нажимает на кнопку -> FAQ
     elif event.text == KBB__USER_M_M__S_K__FAQ:
         vk.messages.send(
             user_id=event.user_id,
-            message='Если ты не найдешь ответа на свой вопрос, ты всегда можешь '
-                    'оставить нам заявку.',
+            message='Если ты не сможешь найти здесь ответа на свой вопрос, '
+                    'ты всегда можешь оставить нам заявку.',
             random_id=random_id(),
-            keyboard=faq_inline_keyboard()
+            keyboard=faq_keyboard()
 
+        )
+
+    # Если запрашивается любой из FAQ вопросов
+    elif event.text == KBB__USER_M_FAQ__COST or \
+            event.text == KBB__USER_M_FAQ__TIME or \
+            event.text == KBB__USER_M_FAQ__HAVE_DESIGN or \
+            event.text == KBB__USER_M_FAQ__EDITS or \
+            event.text == KBB__USER_M_FAQ__PORTFOLIO:
+
+        vk.messages.send(
+            user_id=event.user_id,
+            message=FAQ_DATA[event.text],
+            random_id=random_id()
         )
 
     # Если человек нажимает на кнопку -> ГЛАВНОЕ МЕНЮ
