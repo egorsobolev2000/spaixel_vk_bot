b6bb548a2fe4c7fd0c7c88873b2c34e5fab13d58
egorsobolev2000 <web-disain@mail.ua>
2020-05-04 11:42:44 +0300

    Закончил с написанием модуля FAQ

--- body/user/modules/faq/user_m_f_btns.py
+++ body/user/modules/faq/user_m_f_btns.py
@@ -1,5 +1,6 @@
-KBB__USER_M_FAQ__COST: str = 'Сколько будет стоить разработка сайта?'
+KBB__USER_M_FAQ__COST: str = 'Сколько будет стоить?'
 KBB__USER_M_FAQ__TIME: str = 'Какие сроки?'
-KBB__USER_M_FAQ__HAVE_DESIGN: str = 'У меня есть дизайн, могу я заказать раработку?'
+KBB__USER_M_FAQ__HAVE_DESIGN: str = 'Если уже есть дизайн?'
 KBB__USER_M_FAQ__EDITS: str = 'Как относитесь к правкам?'
+KBB__USER_M_FAQ__PORTFOLIO: str = 'Где я могу увидеть портфолио?'
 
--- body/user/modules/faq/user_m_f_keyboard.py
+++ body/user/modules/faq/user_m_f_keyboard.py
@@ -1,24 +1,29 @@
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 from .user_m_f_btns import *
+from debug.debug import log_error
 
 
-def faq_inline_keyboard():
-    """ Функция создает INLINE
-        клавиатуру для модуля FAQ """
+@log_error
+def faq_keyboard():
+    """ Функция создает клавиатуру
+        для модуля FAQ """
 
-    keyboard = VkKeyboard(one_time=True, inline=True)
+    keyboard = VkKeyboard(one_time=False, inline=True)
 
-    keyboard.add_button(KBB__USER_M_FAQ__COST, color=VkKeyboardColor.DEFAULT)
+    keyboard.add_button(KBB__USER_M_FAQ__TIME, color=VkKeyboardColor.DEFAULT)
 
     keyboard.add_line()
-    keyboard.add_button(KBB__USER_M_FAQ__TIME, color=VkKeyboardColor.DEFAULT)
+    keyboard.add_button(KBB__USER_M_FAQ__COST, color=VkKeyboardColor.DEFAULT)
 
     keyboard.add_line()
     keyboard.add_button(KBB__USER_M_FAQ__HAVE_DESIGN, color=VkKeyboardColor.DEFAULT)
 
     keyboard.add_line()
     keyboard.add_button(KBB__USER_M_FAQ__EDITS, color=VkKeyboardColor.DEFAULT)
+
+    keyboard.add_line()
+    keyboard.add_button(KBB__USER_M_FAQ__PORTFOLIO, color=VkKeyboardColor.DEFAULT)
 
     return keyboard.get_keyboard()
 
