3335d94ed1788ad46b1c97d3dafae0284aedd10c
egorsobolev2000 <web-disain@mail.ua>
2020-05-06 13:38:32 +0300

    Набор кнопок для раздела FAQ

--- body/user/modules/faq/user_m_f_btns.py
+++ body/user/modules/faq/user_m_f_btns.py
@@ -3,4 +3,3 @@ KBB__USER_M_FAQ__TIME: str = 'Какие сроки?'
 KBB__USER_M_FAQ__HAVE_DESIGN: str = 'Если уже есть дизайн?'
 KBB__USER_M_FAQ__EDITS: str = 'Как относитесь к правкам?'
 KBB__USER_M_FAQ__PORTFOLIO: str = 'Где я могу увидеть портфолио?'
-
