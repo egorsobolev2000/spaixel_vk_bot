2450d343246d1ff6aacd2b4bc43bab36b0e1ff0a
egorsobolev2000 <web-disain@mail.ua>
2020-05-06 13:40:56 +0300

    Добавил обработку админских команд

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import vk_api
 
+from brain.admin.main_admin_handler import main_admin_handler
 from config import TOKEN
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from brain.user.main_user_handler import main_user_handler
+from data.ww_json.work_with_json import JSONFile
 from debug.color import ColorsPrint
 
 vk_session = vk_api.VkApi(token=TOKEN)
@@ -21,7 +23,11 @@ if __name__ == "__main__":
             print(f'\nСоединение с Дред — ', ColorsPrint('OK', 'suc').do_colored())
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                    main_user_handler(vk, event)
+                    admin_file = JSONFile(f'./brain/admin/admin.json', d_or_l='load')
+                    if admin_file['status'] == 'ON':
+                        main_admin_handler(vk, event)
+                    else:
+                        main_user_handler(vk, event)
         except Exception as e:
             print(ColorsPrint('Не удалось запустить бота', 'err').do_colored(), e)
         except KeyboardInterrupt:
