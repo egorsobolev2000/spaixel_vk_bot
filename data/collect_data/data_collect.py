43024365dc5e9591651f6471ebe2d96f45f2ef4a
egorsobolev2000 <web-disain@mail.ua>
2020-05-06 13:39:58 +0300

    Прослушиватель событий

--- data/collect_data/data_collect.py
+++ data/collect_data/data_collect.py
@@ -1,9 +1,20 @@
 import os
 import datetime
 
+from brain.brain_base import random_id, check_email
 from debug.debug import log_error
 from data.collect_data.vk_user import VkUser as User
 from data.ww_json.work_with_json import JSONFile
+from body.user.modules.menu.s_keyboard.user_m_m_s_keyboard import standard_keyboard
+
+from debug.debug import timeit
+
+
+def create_user_log(user_info, user_name, msg, date):
+    user_info.update({'message': []})
+    user_info.update({'mail_count': []})
+    user_info['message'] = msg
+    JSONFile(f'./data/logs/{user_name}${date}.json', user_info)
 
 
 def ww_logs(lst, filename, users=False, file=False):
@@ -35,9 +46,16 @@ def data_collector(f):
         date = datetime.datetime.today()
         msg = {f'{date.hour}:{date.minute}:{date.second}': event.text}
         if user_name not in ww_logs(os.listdir('./data/logs/'), user_name, users=True):
-            user_info.update({'message': []})
-            user_info['message'] = msg
-            JSONFile(f'./data/logs/{user_name}${date}.json', user_info)
+            create_user_log(user_info, user_name, msg, date)
+            vk.messages.send(
+                user_id=event.user_id,
+                message=f'Привет, {user_info["first_name"]} 🖐🏿',
+                random_id=random_id(),
+                keyboard=standard_keyboard()
+            )
+
+            check_email(vk, event, 0, '')
+
         else:
             file = ww_logs(os.listdir('./data/logs/'), user_name, file=True)
             user_info = JSONFile(f'./data/logs/{file}', d_or_l='load')
